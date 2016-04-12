e1f8385e55a58058fd07fd9c7de1956758cae033
Alexander Larsson <alexl@redhat.com>
2016-04-12 19:15:21 +0200

    Update to stable releases

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ APPID=`basename $FILE .json`
 
 echo ========== Building $APPID ================
 rm -rf app
-xdg-app-builder --ccache --require-changes --repo=repo --subject="Nightly build of ${APPID}, `date`" ${EXPORT_ARGS-} app $FILE
+xdg-app-builder --ccache --require-changes --repo=repo --subject="Stable build of ${APPID}, `date`" ${EXPORT_ARGS-} app $FILE
